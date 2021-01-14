1427bfed23e10a68090e57c2cc821ea1370da9cf
Yurii Maksimov <yurimaksim@gmail.com>
2021-01-14 20:02:49 +0200

    Task88: Merge two sorted arrays, Task118: Pascal Triangle

---
+++ src/LeetCode/111..120/Task118.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode._81._90
+{
+    public class Task118
+    {
+        public static IEnumerable<object[]> TestData => new List<object[]>
+        {
+            new object[] { -2, new int[][] {new int[] { 1 }}},
+            new object[] { 1, new int[][] {new int[] { 1 }}},
+            new object[] { 2, new int[][]
+                {
+                    new int[] { 1 },
+                    new int[] { 1, 1 }
+                }},
+            new object[] { 3, new int[][]
+            {
+                new int[] { 1 },
+                new int[] { 1, 1 },
+                new int[] { 1, 2, 1 }
+            }},
+            new object[] { 4, new int[][]
+            {
+                new int[] { 1 },
+                new int[] { 1, 1 },
+                new int[] { 1, 2, 1 },
+                new int[] { 1, 3, 3, 1 }
+            }}
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Run(int numRows, IList<IList<int>> expected)
+        {
+            var actual = Generate(numRows);
+            Assert.Equal(actual, expected);
+        }
+
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> triangle = new List<IList<int>>();
+
+            if (numRows == 0) return triangle;
+        
+            IList<int> initRow = new List<int>();
+            initRow.Add(1);
+            triangle.Add(initRow);
+        
+            for (int i = 1; i < numRows; i++)
+            {            
+                IList<int> row = new List<int>();
+                IList<int> lastRow = triangle[i - 1];
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    int lastRow_prelast_cell = (j - 1 < 0) ? 0 : lastRow[j - 1];
+                    int lastRow_last_cell =    (j < 0) ? 0 : lastRow[j];
+                    row.Add(lastRow_prelast_cell + lastRow_last_cell);
+                }
+                row.Add(1);
+                triangle.Add(row);
+            }
+
+            return triangle;
+        }
+    }
+}
---
+++ src/LeetCode/81..90/Task88.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace LeetCode._81._90
+{
+    public class Task88
+    {
+        [Theory]
+        [InlineData(new[] {1,2,3,0,0,0}, 3, new[] {2,5,6}, 3,new[] {1,2,2,3,5,6})] 
+        [InlineData(new[] {5,6,0,0}, 2, new[] {2,3}, 2, new[] {2,3,5,6})]
+        [InlineData(new[] {1,2,0,0}, 2, new[] {3,4}, 2, new[] {1,2,3,4})]
+        public void Run(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+            Merge(nums1, m, nums2, n);
+            Assert.Equal(nums1, expected);
+        }
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        
+            int t = nums1.Length - 1;
+            m--;
+            n--;
+            while (t>= 0 && (m >= 0 || n >= 0))
+            {
+                if (m < 0)
+                {
+                    for (int i = 0; i <= n; i++)
+                    {
+                        nums1[i] = nums2[i];
+                    }
+                    break;
+                }
+            
+                if (n < 0)
+                {
+                    break;
+                }
+            
+                if (nums1[m] <= nums2[n])
+                {
+                    nums1[t] = nums2[n];
+                    n--;
+                    t--;
+                    continue;
+                }
+            
+                if (nums1[m] > nums2[n])
+                {
+                    nums1[t] = nums1[m];
+                    m--;
+                    t--;
+                }
+            }
+        }        
+    }
+}
